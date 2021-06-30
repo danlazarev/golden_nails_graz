0eff1997a9b6755acf2e826af5bdfe24de9a8ef1
Dan <79512029+danlazarev@users.noreply.github.com>
2021-06-30 22:39:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+var scene_1 = document.getElementById('scene-1');
+var parallaxInstance = new Parallax(scene_1);
